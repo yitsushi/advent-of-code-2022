14ef58ce901d5f7a5dc68ec43919611b8e4911bf
Efertone <efertone@pm.me>
2022-12-03 11:43:41 +0100

    day03: part1 and part2

---
+++ lib/solution/src/day03/readme.md
@@ -0,0 +1,25 @@
+# Day 3
+
+## Part 1
+
+```bash
+cargo run -- --day=3 --part=1 --time-it
+```
+
+```text
+ -- Bootstrap solver: 243.5µs
+ -- Solution: 5.1966ms
+7831
+```
+
+## Part 2
+
+```bash
+cargo run -- --day=3 --part=2 --time-it
+```
+
+```text
+ -- Bootstrap solver: 220.2µs
+ -- Solution: 9.7261ms
+2683
+```
--- template/day_mod.rs
+++ template/day_mod.rs
@@ -2,15 +2,15 @@
 pub struct Solution { }
 
 impl aoc::Solver for Solution {
+    fn read_lines(&mut self, _: Vec<String>) {
+        todo!()
+    }
+
     fn part1(&self) -> String {
         todo!()
     }
 
     fn part2(&self) -> String {
-        todo!()
-    }
-
-    fn read_lines(&mut self, _: Vec<String>) {
         todo!()
     }
 }
@@ -23,6 +23,7 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
+    use aoc::io::{Filesystem, LocalFilesystem};
     use aoc::Solver;
 
     use super::Solution;
@@ -31,7 +32,7 @@ mod tests {
     fn example1() {
         let fs = LocalFilesystem{};
         let mut solver = Solution::new();
-        solver.read_lines(fs.read_file("tests/fixtures/empty"));
+        solver.read_lines(fs.read_file("tests/fixtures/empty").unwrap());
 
         assert_eq!(solver.part1(), format!("{}", 0));
         assert_eq!(solver.part2(), format!("{}", 0));
