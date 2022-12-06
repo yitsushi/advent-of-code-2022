149b76c40639e5f7d0629e44ef572c5dc270b2a1
yitsushi <yitsushi@users.noreply.github.com>
2022-12-06 09:58:49 +0000

    deploy: 8fee1435694bc3638ce88dd2eb533e3b642732e1

---
+++ solution/day06/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["Solution","Day 6: Tuning Trouble"]]};
--- solution/sidebar-items.js
+++ solution/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["day01",""],["day02",""],["day03",""],["day04",""],["day05",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["day01",""],["day02",""],["day03",""],["day04",""],["day05",""],["day06",""]]};
